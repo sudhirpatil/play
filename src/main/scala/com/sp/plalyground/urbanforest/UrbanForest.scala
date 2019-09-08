dfd2eba88f6c0724451ed2ec73ab60f779c633ae
sudhirpatil <spatil.sudhir@gmail.com>
2019-09-08 16:53:42 +0800

    comments

--- src/main/scala/com/sp/plalyground/urbanforest/UrbanForest.scala
+++ src/main/scala/com/sp/plalyground/urbanforest/UrbanForest.scala
@@ -34,38 +34,42 @@ object UrbanForest {
       println("Insufficient Arguments, please provide: <stat area file path> <forest file path>")
       System.exit(-1)
     }
-
     val statAreaFilePath = args(0)
     val forestFilePath = args(1)
-    val forestDf = readForestFiles(forestFilePath) //"/Users/sudhirpatil/code/urbanforest/src/main/Resources/challenge-urban-forest/melb_urban_forest_2016.txt/part-00000")
-    //: RDD[(String, MultiPolygon)]
-    val forestPolygons: RDD[(String, MultiPolygon)] = getForestPolygons(forestDf.limit(10))
+
+    // Parse forest text files and get RDD with MultiPolygons
+    val forestPolygons: RDD[(String, MultiPolygon)] = parseForestFiles(forestFilePath) //"/Users/sudhirpatil/code/urbanforest/src/main/Resources/challenge-urban-forest/melb_urban_forest_2016.txt/part-00000")
+    // Read Stat Area json & agg for coordinates and area at Level 2 Statistical Areas
     val statLevel2Df = getStatAreaLevel2(statAreaFilePath) //"/Users/sudhirpatil/code/urbanforest/src/main/Resources/challenge-urban-forest/melb_inner_2016.json")
+    // Convert Stat Area coordinates to Multipolygon
     val statAreaPolygons: RDD[(String, MultiPolygon)] = getStatAreaPolygons(statLevel2Df)
-    val greenAreaDf = getGreenArea(statAreaPolygons, forestPolygons)
-    val greenestAreas = getGreenestAreas(statLevel2Df, greenAreaDf, 5)
+    val greenAreaDf = getGreenAreaInSA(statAreaPolygons, forestPolygons)
+    val greenestAreas = getGreenestAreas(statLevel2Df, greenAreaDf)
     greenestAreas.show(false)
   }
 
-  def readForestFiles(forestFilePath: String): DataFrame = {
-    spark.sparkContext.setLogLevel("WARN")
-    // Create RDD Multipolygons from urban forest text file
-    spark.read.
+  // Parse forest text files and get RDD with MultiPolygons
+  def parseForestFiles(forestFilePath: String): RDD[(String, MultiPolygon)] = {
+    // Create dataframe from text file
+    val forestDf = spark.read.
       option("quote", "\"").
       option("escape", "\"").
       option("sep", " ").
       option("ignoreLeadingWhiteSpace", true).
       csv(forestFilePath)
+
+    // Create RDD Multipolygons from Dataframe
+    getForestPolygons(forestDf.limit(10))
   }
 
-  // get RDD (index, Multipolygons)
+  // get RDD (index, Multipolygons) from Dataframe
   def getForestPolygons(forestDf: DataFrame): RDD[(String, MultiPolygon)] ={
     forestDf.rdd.map(row => {
       val index = row.getString(0)
       val polyPoints = row.getString(1)
       // Pattern to Extract each loop in polygon as string
       val pattern = """\(.*?\)""".r
-      val polygon: Polygon = pattern.
+      val polygon: Seq[Loop] = pattern.
         // Extract each loop in polygon as string
         findAllIn(polyPoints).map(x => x.replace("(","").replace(")", "")).
         map(loopStr => {
@@ -81,14 +85,12 @@ object UrbanForest {
     })
   }
 
+  // Read Stat Area json & agg for coordinates and area at sa2 level
   def getStatAreaLevel2(statFilePath: String): DataFrame = {
-    // get multipolygons for each area at level 2 of stat area
-    val statAreaDf = spark.read.json(statFilePath)
-    statAreaDf.printSchema()
-    statAreaDf.show(1, false)
-    // agg at sa2 level
-    statAreaDf.groupBy("sa2_main16", "sa2_5dig16", "sa2_name16").
-      agg(collect_list("geometry.coordinates").alias("seq_multipolygon"), sum("areasqkm16").alias("areasqkm16"))
+    spark.read.json(statFilePath).
+      groupBy("sa2_main16", "sa2_5dig16", "sa2_name16").
+        agg(collect_list("geometry.coordinates").alias("seq_multipolygon"),
+          sum("areasqkm16").alias("areasqkm16"))
   }
 
   // Convert coordinates to Multipolygon from grouped area at sa2
@@ -101,27 +103,31 @@ object UrbanForest {
     })
   }
 
-  def getGreenArea(statAreaPolygons:RDD[(String, MultiPolygon)], forestPolygons: RDD[(String, MultiPolygon)]): DataFrame = {
-    // RDD with broadcast join
+  // Get forest area in each Suburb
+  def getGreenAreaInSA(statAreaPolygons:RDD[(String, MultiPolygon)], forestPolygons: RDD[(String, MultiPolygon)]): DataFrame = {
+    // Broadcast Stat Area RDD as size small compared to forest data
     val statAreaBcast = sc.broadcast(statAreaPolygons.collectAsMap())
-    forestPolygons.flatMap(forestRow => {
-      val fmPolygon: MultiPolygon = forestRow._2
-      //    println(s"Starting forest : ${forestRow._1}")
-      statAreaBcast.value.
-        filter(statArea => mayIntersect(fmPolygon, statArea._2)).
-        map(statArea => (statArea._1, intersectionArea(statArea._2, fmPolygon))).
-        filter(statArea => statArea._2 > 0.0)
-    }).
-    toDF("sa2_main16", "intersect_area").
-    groupBy("sa2_main16").agg(sum("intersect_area").alias("green_area"))
+
+    // First get intersection for each forest & stat area, then agg for each stat area
+    forestPolygons.
+      flatMap(forestRow => { // Multiple records for each forest & stat area
+        val fmPolygon: MultiPolygon = forestRow._2
+        // Get intersection area Stat area using broadcast value
+        statAreaBcast.value.
+          filter(statArea => mayIntersect(fmPolygon, statArea._2)).
+          map(statArea => (statArea._1, intersectionArea(statArea._2, fmPolygon))).
+          filter(statArea => statArea._2 > 0.0)
+      }).
+      toDF("sa2_main16", "intersect_area").
+      //agg intersect value at each stat area
+      groupBy("sa2_main16").agg(sum("intersect_area").alias("green_area"))
   }
 
-  def getGreenestAreas(statLevel2Df: DataFrame, greenAreaDf: DataFrame, limit: Int = 10): DataFrame = {
+  def getGreenestAreas(statLevel2Df: DataFrame, greenAreaDf: DataFrame): DataFrame = {
     statLevel2Df.join(greenAreaDf, "sa2_main16").
       select("sa2_main16", "sa2_5dig16", "sa2_name16", "areasqkm16", "green_area").
       withColumn("green_area_ratio", col("green_area") / col("areasqkm16")).
-      orderBy(desc("green_area_ratio")).
-      limit(limit)
+      orderBy(desc("green_area_ratio"))
   }
 
 }
